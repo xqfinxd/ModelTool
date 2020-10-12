6a1ec9fa86c59206ea3376f4eb4a8760e866876a
xqfinxd <xqf2625513819@qq.com>
2020-10-13 00:36:42 +0800

    控制台项目

---
+++ ModelTool/ModelUtil.cs
@@ -0,0 +1,24 @@
+﻿using Assimp;
+using System.IO;
+
+namespace ModelTool
+{
+    internal class ModelUtil
+    {
+        public Scene scene = new Scene();
+
+        public ModelUtil(string file)
+        {
+            AssimpContext importer = new AssimpContext();
+            FileInfo info = new FileInfo(file);
+
+            if (info.Exists && importer.IsImportFormatSupported(info.Extension))
+            {
+                scene = importer.ImportFile(file,
+                    PostProcessSteps.GenerateNormals |
+                    PostProcessSteps.CalculateTangentSpace |
+                    PostProcessSteps.Triangulate);
+            }
+        }
+    }
+}
